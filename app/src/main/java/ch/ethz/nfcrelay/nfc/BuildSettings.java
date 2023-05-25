80b68a36c4871b5c2c395c465bad6fb94942520a
Daniele Coppola <dcoppola@ethz.ch>
2023-05-25 18:31:23 +0200

    removed ignored files

--- app/src/main/java/ch/ethz/nfcrelay/nfc/BuildSettings.java
+++
@@ -1,8 +0,0 @@
-package ch.ethz.nfcrelay.nfc;
-
-public class BuildSettings {
-    public static final boolean mockBackend = true;
-    public static final boolean mockUart = true;
-    public static final boolean transparentRelay = true;
-    public static final boolean checkRemoteConnection = true;
-}
