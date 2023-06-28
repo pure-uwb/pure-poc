fd048e17ccda1d944ed6a5ff7ffe9ef32d914cbd
Daniele Coppola <dcoppola@ethz.ch>
2023-06-28 17:38:21 +0200

    improved reset scripts.

--- adb_scripts/restart.sh
+++ adb_scripts/restart.sh
@@ -4,9 +4,11 @@ if [ $# -ne 2 ];then
 fi
 apk=$1
 activity=$2
-ids=$(adb devices -l | tail -n +2 | cut -d ":" -f 5)
+#ids=$(adb devices -l | tail -n +2 | cut -d ":" -f 5)
+ids=$(adb devices -l | tail -n +2 | tr -s " " | cut -d " " -f 1)
+
 for id in $ids; do	
 	echo "Restart on device $id"
-	adb -t $id  install $apk
-	adb -t $id  shell am start -n $activity  -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+	adb -s $id  install $apk
+	adb -s $id  shell am start -n $activity  -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
 done	
